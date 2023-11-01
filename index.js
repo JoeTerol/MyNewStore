3c703976cfe6b18d1d6d0707f9a9123b136b3e2f
JoeTerol <teroljoseeduardo@gmail.com>
2023-10-31 21:40:01 -0300

    feat: cors installed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
 
 const { logErrors, boomErrorHandler, errorHandler } = require('./middlewares/error.handler')
@@ -6,6 +7,19 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+const whitelist = ['http://localhost:8080'];
+  const options = {
+    origin: (origin, callback) => {
+      if (whitelist.includes(origin)) {
+        callback(null, true)
+
+      }else {
+        callback(new Error('No Permitido'))
+      }
+    }
+  };
+app.use(cors(options));
+
 
 routerApi(app);
 
